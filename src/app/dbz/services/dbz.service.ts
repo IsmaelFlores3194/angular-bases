ebd96d6ec201f3d00e291988b2e2947a06a0c1fc
Ismael <X268268@MXMH011100>
2024-08-20 15:25:05 -0600

    Scripts auto dist -deploy-

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -3,41 +3,42 @@ import { Character } from '../interfaces/character.interface';
 import { v4 as uuid } from 'uuid';
 
 @Injectable({
-  providedIn: 'root'})
+  providedIn: 'root'
+})
 export class DbzService {
 
-  public characters :  Character[] =  [{
+  public characters: Character[] = [{
     id: uuid(),
-    name : 'Krillin',
-    power : 1000
-  },{
+    name: 'Krillin',
+    power: 1000
+  }, {
     id: uuid(),
-    name : 'Goku',
-    power : 9500
+    name: 'Goku',
+    power: 9500
   }];
 
 
 
 
-  addCharacter(character: Character) : void {
+  addCharacter(character: Character): void {
 
     // const  newCharacter : Character = {
     //   id : uuid(),
     //   name : character.name,
     //   power:character.power
     // }
-    const newCharacter : Character = { id: uuid(), ...character}
+    const newCharacter: Character = { id: uuid(), ...character }
 
     this.characters.push(newCharacter);
   }
 
 
-    // onDeleteCharacter(index : number){
-    //      this.characters.splice(index, 1);
-    // }
-    onDeleteCharacterById(id : string){
-         this.characters = this.characters.filter( character => character.id  !== id);
-    }
+  // onDeleteCharacter(index : number){
+  //      this.characters.splice(index, 1);
+  // }
+  onDeleteCharacterById(id: string) {
+    this.characters = this.characters.filter(character => character.id !== id);
+  }
 
 
 
@@ -48,6 +49,6 @@ export class DbzService {
   constructor() {
 
 
-   }
+  }
 
 }
